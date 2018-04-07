0cab593e20ec33a7d26fa9dc8933528f02c728a3
Carol Enache <enache.carol@gmail.com>
2018-04-07 23:59:23 +0100

    initiate the action creators and define the type of actions

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,29 @@
+//increment
+
+export function increment(indexe) {
+    return {
+        type: 'INCREMENT_LIKES',
+        index
+    };
+};
+
+//add comment
+
+export function addComment(postId, author, comment) {
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        author,
+        comment
+    }
+}
+
+// remove comment
+
+export function removeComment(postId, i) {
+    return {
+        type: 'REMOVE_COMMENT',
+        i,
+        postId
+    }
+}
