20f161e7461bd347efadc5241e6a97b97c83ad99
Carol Enache <enache.carol@gmail.com>
2018-04-08 14:51:57 +0100

    fix images path and create Photo component HTML structure

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 const Photo = React.createClass({
+
     render(){
+        const { post, i, comments } = this.props;
         return (
             <figure className='grid-figure'>
-                I am a photo
+                <div className='grid-photo-wrapp'>
+                    <Link to={`/view/${post.code}`}>
+                        <img src={post.display_src} alt={post.caption} className='grid-photo'/>
+                    </Link>
+                    <ReactCSSTransitionGroup transitionName='like'
+                        transitionEnterTimeout={500}
+                        transitionLeaveTimeout={500}
+                        ><span key={post.like} className='likes-heart'>
+                            {post.like}
+                        </span>
+                    </ReactCSSTransitionGroup>
+                </div>
+                <figcaption>
+                    <p>{post.caption}</p>
+                    <div className='control-buttons'>
+                        <button className='likes'>&hearts; {post.likes}</button>
+                        <Link to={`/view/${post.code}`} className='button'>
+                            <span className='comment-count'>
+                                <span className='speech-bubble'></span>
+                                {comments[post.code] ? comments[post.code].length : 0}
+                            </span>
+                        </Link>
+                    </div>
+                </figcaption>
             </figure>
         )
     }
